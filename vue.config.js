85653a22c6b7c21044f8e30603659553229eef68
Ivan Tsyganov <insdevmail@gmail.com>
2020-03-04 00:05:43 +0200

    add style resource loader

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: ['@/assets/styles/base/variables.scss', '@/assets/styles/base/function.scss'],
+    },
+  },
+};
