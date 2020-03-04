ce39171addc11bcfb9aee319e76d9e90a408ab4b
Ivan Tsyganov <insdevmail@gmail.com>
2020-03-05 01:51:27 +0200

    move project toolbar to separate file

---
+++ blocks/project/ProjectToolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="[$style['toolbar']]" class="p-3 bg-gray-800">
+    <div class="w-3/12">
+      <CToolbarButton icon="solid/folder-open" class="mr-3">
+        Open project
+      </CToolbarButton>
+      <CToolbarButton icon="regular/save">
+        Save
+      </CToolbarButton>
+    </div>
+    <div class="w-3/12">
+      <CToolbarButton icon="solid/plus-square" class="mr-3">
+        Add ID
+      </CToolbarButton>
+      <CToolbarButton icon="solid/minus-square">
+        Remove ID
+      </CToolbarButton>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectToolbar',
+};
+</script>
+
+<style lang="scss" module>
+.toolbar {
+  display: flex;
+}
+</style>
--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,34 +1,18 @@
 <template>
   <div>
-    <div :class="[$style['top']]" class="p-3 bg-gray-800">
-      <div class="w-3/12">
-        <CToolbarButton icon="solid/folder-open" class="mr-3">
-          Open project
-        </CToolbarButton>
-        <CToolbarButton icon="regular/save">
-          Save
-        </CToolbarButton>
-      </div>
-      <div class="w-3/12">
-        <CToolbarButton icon="solid/plus-square" class="mr-3">
-          Add ID
-        </CToolbarButton>
-        <CToolbarButton icon="solid/plus-square">
-          Remove ID
-        </CToolbarButton>
-      </div>
-    </div>
+    <ProjectToolbar />
   </div>
 </template>
 
 <script>
+import ProjectToolbar from '../../blocks/project/ProjectToolbar';
+
 export default {
   name: 'PageProject',
+  components: {
+    ProjectToolbar,
+  },
 };
 </script>
 
-<style lang="scss" module>
-.top {
-  display: flex;
-}
-</style>
+<style lang="scss" module></style>
