3b474981c922adf0ea1fcb4f846e1b85da790f07
Ivan Tsyganov <insdevmail@gmail.com>
2020-03-04 02:08:07 +0200

    add table

--- src/components/popup/PopupNewProject.vue
+++ src/components/popup/PopupNewProject.vue
@@ -1,6 +1,22 @@
 <template>
   <Modal name="new-project">
-    <div class="text-center font-semibold text-2xl">New project configuration</div>
+    <div class="text-center font-semibold text-2xl mb-6">New project configuration</div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th class="">Lang</th>
+          <th class="">Path</th>
+          <th class="">Primary</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">English</th>
+          <td>C:/path/to/file.json</td>
+          <td>Otto</td>
+        </tr>
+      </tbody>
+    </table>
   </Modal>
 </template>
 
