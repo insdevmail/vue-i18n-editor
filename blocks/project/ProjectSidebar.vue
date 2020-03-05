0e93cf6d676941dd9aa84bd6f685690aeac2257f
Ivan Tsyganov <insdevmail@gmail.com>
2020-03-05 02:00:20 +0200

    add project content block

---
+++ blocks/project/ProjectSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="[$style['sidebar']]" class="bg-gray-800 p-3">
+    Sidebar
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectSidebar',
+};
+</script>
+
+<style lang="scss" module>
+.sidebar {
+  border-top: 1px solid theme('colors.gray.900');
+}
+</style>
--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,18 +1,41 @@
 <template>
-  <div>
+  <div class="page">
     <ProjectToolbar />
+    <div :class="[$style['content']]">
+      <ProjectSidebar class="w-3/12" />
+      <div :class="[$style['main']]" class="w-9/12">
+        <div :class="[$style['main__top']]" class="py-1 px-2 text-sm">
+          Translations
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ProjectToolbar from '../../blocks/project/ProjectToolbar';
+import ProjectSidebar from '../../blocks/project/ProjectSidebar';
 
 export default {
   name: 'PageProject',
   components: {
     ProjectToolbar,
+    ProjectSidebar,
   },
 };
 </script>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+.content {
+  flex-grow: 1;
+  display: flex;
+}
+
+.main {
+  &__top {
+    @apply bg-gray-800;
+    border-left: 1px solid theme('colors.gray.900');
+    border-top: 1px solid theme('colors.gray.900');
+  }
+}
+</style>
